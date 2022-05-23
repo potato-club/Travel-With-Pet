387959aea798a9a57045736ccdae8318a7b6cea9
UNNEW-main <1970005826c@gmail.com>
2022-05-23 14:22:34 +0900

    test: login Link태그 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from 'next';
 import { IntroMainPage } from 'src/containers/index';
-
+import Link from 'next/link';
 const Home: NextPage = () => {
-  return <IntroMainPage />;
+  return (
+    <div>
+      <Link
+        href={
+          'http://ec2-3-37-80-15.ap-northeast-2.compute.amazonaws.com:4000/google'
+        }
+      >
+        <a>구글 로그인하기</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
