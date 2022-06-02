c8b392100ba555a55c3260d28bcdceb4b13ba357
eunnyoung <12dreamer30@gmail.com>
2022-06-02 16:35:58 +0900

    chore: MyPosts 불필요한 주석 삭제

--- src/containers/mypage/components/MyPosts.tsx
+++ src/containers/mypage/components/MyPosts.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import { PhotoCardList } from 'src/components';
-import styled from 'styled-components';
-import { TypoGraphy } from '../../../components/TypoGraphy';
-import { customColor } from 'src/constants';
 import { InformationBoard } from 'src/dummy/photoCardInfo';
 import { TabsCategory } from './TabsCategory';
-import Pagination from 'react-js-pagination';
 import { CustomPagination } from 'src/components/CustomPagination';
 import { InformationBoardType } from 'src/types/board.type';
 
@@ -42,46 +38,3 @@ export const MyPosts = () => {
     </>
   );
 };
-
-// 이상하게 다 날라간 코드!
-// import React, { useState } from 'react';
-// import { PhotoCardList } from 'src/components';
-// import styled from 'styled-components';
-// import { TypoGraphy } from '../../../components/TypoGraphy';
-// import { customColor } from 'src/constants';
-// import { InformationBoard } from 'src/dummy/photoCardInfo';
-// import { TabsCategory } from './TabsCategory';
-// import Pagination from 'react-js-pagination';
-// import { CustomPagination } from 'src/components/CustomPagination';
-// import { InformationBoardType } from 'src/types/board.type';
-
-// export const MyPosts = () => {
-//   const [activePage, setActivePage] = useState<number>(1);
-
-//   // categoryData를 일단 다른 걸로 바꿔본다!
-//   const photoCardInfoPiece: InformationBoardType[] = [];
-//   const postData: InformationBoardType[] = [];
-//   const pieceNumber = 9;
-//   postData.map((data, index) => {
-//     if (
-//       index < (activePage - 1) * pieceNumber ||
-//       index > activePage * pieceNumber - 1
-//     ) {
-//       return;
-//     }
-//     photoCardInfoPiece.push(data);
-//   });
-
-//   return (
-//     <>
-//       <TabsCategory text={'내가 작성한 글'} />
-//       <PhotoCardList type="MyPage" photoCardInfo={photoCardInfoPiece} />
-//       <CustomPagination
-//         activePage={activePage}
-//         itemsCountPerPage={pieceNumber}
-//         totalItemsCount={postData.length}
-//         onChange={e => setActivePage(e)}
-//       />
-//     </>
-//   );
-// };
