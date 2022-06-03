d658bd7dac8d82b4c13be1534c5af92fdec36812
KoJaem <woalswhwh@naver.com>
2022-06-03 15:05:04 +0900

    style : 로딩 디자인 구현

--- src/context/LoadingContext.tsx
+++ src/context/LoadingContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, ReactNode, useState } from 'react';
 import styled from 'styled-components';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { customColor } from 'src/constants';
 
 export const LoadingProvider = createContext({
   onLoading: () => {},
@@ -21,17 +23,40 @@ export const LoadingContext = ({ children }: Props) => {
   };
 
   return (
-      <LoadingProvider.Provider value={{ onLoading, offLoading }}>
-        {isLoading && <Loading />}
-        {children}
-      </LoadingProvider.Provider>
+    <LoadingProvider.Provider value={{ onLoading, offLoading }}>
+      {isLoading && (
+        <LoadingContainer>
+          <SpinnerWrapper>
+            <LoadingSpinner color={customColor.brown} size={200} />
+          </SpinnerWrapper>
+        </LoadingContainer>
+      )}
+      {children}
+    </LoadingProvider.Provider>
   );
 };
 
-const Loading = styled.div`
+const LoadingContainer = styled.div`
   width: 100%;
   height: 100%;
   position: fixed;
-  background-color: #28aeec80;
+  background-color: #35393b80;
   z-index: 100;
+`;
+const SpinnerWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+const LoadingSpinner = styled(AiOutlineLoading3Quarters)`
+  animation: spin 2s linear infinite;
+  @keyframes spin {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
