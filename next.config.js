c9ab0037ac471f3b102a70d9bec89735c5f4e348
KoJaem <woalswhwh@naver.com>
2022-06-11 17:33:35 +0900

    feat : 외부 image 를 가져오기 위한 config 작업

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  target: 'serverless',
+  // target: 'serverless',
   reactStrictMode: true,
   images: {
-    loader: 'akamai',
-    path: '/',
+    // loader: 'akamai',
+    // path: '/',
+    domains: ['meongmeong-tui-img-storage.s3-ap-northeast-2.amazonaws.com'],
   },
 };
 
